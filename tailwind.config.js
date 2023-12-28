9fa3896bc1d0793dae00cda4fcbb7d46dc433399
oseji <jiade1233@gmail.com>
2023-12-28 18:07:35 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      width: {
+        "45/100": "45%",
+      },
+    },
+  },
+  plugins: [],
+};
